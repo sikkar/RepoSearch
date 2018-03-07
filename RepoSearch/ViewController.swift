19a9046394919b130fa21e048d4c471e32fa5ab6
Angel Escribano <angel.escribano@t-systems.com>
2018-03-07 12:59:04 +0100

    Initial Commit

---
+++ RepoSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RepoSearch
+//
+//  Created by Angel Escribano on 7/3/18.
+//  Copyright © 2018 -. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
